6c3b9d444844efd4ce52681e63316bf758e23c3e
egarcia07x <31666808+egarcia07x@users.noreply.github.com>
2017-09-10 03:03:10 -0400

    1.0

---
+++ resume.js
@@ -0,0 +1,41 @@
+var name = "elias garcia";
+var capName= name.toUpperCase()
+console.log("Name:"+ capName)
+
+var age = "Age:23"
+console.log(age)
+
+var career = "Career:Currently a Student at Covalence, Pursing a Career in Web Devlopement"
+console.log(career)
+
+var bio = "Short Description: I was born in Los Angeles California, but was raised in Chattanooga,TN."
+console.log(bio)
+
+var a="Consist of Hiking,"
+var b="reading,"
+var c="photography"
+var d="travling,"
+var e="learning how to program in javascript,"
+var f="and anything adventurous!"
+console.log("My Interests:"+ a,b,c,d,e,f)
+
+console.log("Experience:")
+
+
+
+
+console.log("My Skills:")
+var skill1="CSS, HTML, Novice JavaScript Coder"
+console.log("")
+var skill2="Bilingual"
+var skill3="Great Team Player"
+var skill4="Nascar Driver"
+
+function displaySkills(s1,s2,s3,s4,isCool) { 
+    if (isCool === true)}
+    
+    console.log(s1,s2,s3,s4)
+}
+
+displaySkills(skill1,skill2,skill3,skill4)
+
